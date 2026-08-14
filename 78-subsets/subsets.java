8436879a9ca652f49ff2b3e4df268792a133fafe
Arsh Grover <student.arsh.grover@gmail.com>
2026-08-15 01:32:55 +0530

    Time: 1 ms (86.43%) | Memory: 44 MB (88.73%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -4,10 +4,13 @@ class Solution {
             ans.add(new ArrayList<>(current));
             return;
         }
+        solve(nums,index+1,current,ans);
         current.add(nums[index]);
+        // take
         solve(nums,index+1,current,ans);
+        // backtracking
         current.remove(current.size()-1);
-        solve(nums,index+1,current,ans);
+        // not take
     }
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>ans=new ArrayList<>();
